3bf8289900ee503bd5a5b1b41380432a40653f4a
刘铭 <1986247287@qq.com>
2018-09-02 19:52:21 +0800

    配置首页

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+
+//导入全局的样式
+// 导入bootstrap的样式
+import 'bootstrap/dist/css/bootstrap.css';
+import './assets/css/index.css'
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
